f37c71e8af08f07a5dd58d8b8df19f9d1b803d14
MooMoong <alchemist72727@gmail.com>
2025-01-15 19:03:20 +0900

    chapter02완료  25.01.15

---
+++ src/Chapter02/Ex2_2.java
@@ -0,0 +1,13 @@
+package Chapter02;
+
+public class Ex2_2 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World");
+		System.out.println("Hello, World");
+		System.out.println("3+5=");
+		System.out.println(3+5);
+//괄호안에 숫자를 넣으면 계산된 결과가 출력되지만 ""(큰따옴표)안에 넣은 내용은 글자로 간주되어 계산되지 않고 있는 그대로 출력된다.
+	}
+
+}
---
+++ src/Chapter02/Ex2_3.java
@@ -0,0 +1,13 @@
+package Chapter02;
+
+public class Ex2_3 {
+
+	public static void main(String[] args) {
+		System.out.println(10+5);
+		System.out.println(10-5);
+		System.out.println(10*5);
+		System.out.println(10/5);
+
+	}
+
+}
