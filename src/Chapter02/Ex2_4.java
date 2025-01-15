776728e40809dd008259683e36e10872ae1949b8
MooMoong <alchemist72727@gmail.com>
2025-01-15 19:10:03 +0900

    테스트

---
+++ src/Chapter02/Ex2_4.java
@@ -0,0 +1,14 @@
+package Chapter02;
+
+public class Ex2_4 {
+
+	public static void main(String[] args) {
+			int x = 10;
+			System.out.println(x);
+			
+			x=15;
+			System.out.println(x);
+
+	}
+
+}
