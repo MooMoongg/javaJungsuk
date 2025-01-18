55367d5ed2492be7a400e1b8bbe457f3f4b35355
MooMoong <alchemist72727@gmail.com>
2025-01-18 17:15:01 +0900

    2025.01.18 chapter04 진행중

---
+++ src/Chapter03/Exercise3_2.java
@@ -0,0 +1,13 @@
+package Chapter03;
+
+public class Exercise3_2 {
+	public static void main(String[] args) {
+		
+		int x = 2;
+		int y = 5;
+		char c = 'A';
+		
+		System.out.println(x += 2);
+		System.out.println('C' - c);
+	}
+}
