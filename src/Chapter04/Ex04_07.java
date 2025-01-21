956466059fd710f4b4b3e35b350c8780cff2e5b6
MooMoong <alchemist72727@gmail.com>
2025-01-21 23:28:28 +0900

    2025.01.21 chapter04 진행중

---
+++ src/Chapter04/Ex04_07.java
@@ -0,0 +1,14 @@
+package Chapter04;
+
+public class Ex04_07 {
+
+	public static void main(String[] args) {
+		int score = 50;
+		
+		if(score > 60) {
+			System.out.println("합격입니다.");
+		}
+
+	}
+
+}
