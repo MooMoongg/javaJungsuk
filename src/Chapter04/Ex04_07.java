568430538478bef45881ea8c73374e7406ca2e3f
MooMoong <alchemist72727@gmail.com>
2025-01-23 20:56:09 +0900

    2025.01.23 chapter04

--- src/Chapter04/Ex04_07.java
+++ src/Chapter04/Ex04_07.java
@@ -3,12 +3,11 @@ package Chapter04;
 public class Ex04_07 {
 
 	public static void main(String[] args) {
-		int score = 50;
-		
-		if(score > 60) {
-			System.out.println("합격입니다.");
-		}
+		int num = 0;
 
+		for (int i = 1; i <= 5; i++);
+		num = (int) (Math.random() * 6) + 1;
+		System.out.println(num);
 	}
 
 }
