022fd075427d7f5ec1b077ed3db1f7be6dbe2563
MooMoong <alchemist72727@gmail.com>
2025-01-25 23:40:45 +0900

    2025.01.25 chapter04

--- src/Chapter04/Ex04_07.java
+++ src/Chapter04/Ex04_07.java
@@ -3,11 +3,10 @@ package Chapter04;
 public class Ex04_07 {
 
 	public static void main(String[] args) {
-		int num = 0;
-
-		for (int i = 1; i <= 5; i++);
-		num = (int) (Math.random() * 6) + 1;
-		System.out.println(num);
+		for(int i = 1 ; i <= 3 ; i++) {
+			System.out.println("Hello");
+		}
+			
 	}
 
 }
---
+++ src/Chapter04/Ex04_08.java
@@ -0,0 +1,13 @@
+package Chapter04;
+
+public class Ex04_08 {
+
+	public static void main(String[] args) {
+		int num = 0;
+
+		for (int i = 1; i <= 5; i++);
+		num = (int) (Math.random() * 6) + 1;
+		System.out.println(num);
+	}
+
+}
