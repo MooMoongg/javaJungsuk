d59e9a1b0c6640b012acf0b25f96fa9db139eeea
MooMoongg <alchemist72727@gmail.com>
2025-02-26 18:20:27 +0900

    feat: 배열의 활용

--- src/Chapter05/Ex05_02.java
+++ src/Chapter05/Ex05_02.java
@@ -3,8 +3,20 @@ package Chapter05;
 public class Ex05_02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
+		int sum = 0;
+		float average = 0f;
+		
+		int[] score = {100, 88, 100, 100, 90};
+		
+		for(int i=0; i<score.length; i++) {
+			sum += score[i];
+		}
+		average = sum/(float)score.length;
+		
+		System.out.println("총합 : "+ sum);
+		System.out.println("평균 : "+ average);
 	}
 
 }
