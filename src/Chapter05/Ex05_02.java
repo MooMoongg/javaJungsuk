18f2e63236c8e27992db345215d61c530b1be786
MooMoongg <alchemist72727@gmail.com>
2025-02-27 19:52:53 +0900

    feat: Chapter05 완료(배열)

--- src/Chapter05/Ex05_02.java
+++ src/Chapter05/Ex05_02.java
@@ -3,20 +3,19 @@ package Chapter05;
 public class Ex05_02 {
 
 	public static void main(String[] args) {
-		
 
 		int sum = 0;
 		float average = 0f;
-		
-		int[] score = {100, 88, 100, 100, 90};
-		
-		for(int i=0; i<score.length; i++) {
+
+		int[] score = { 100, 88, 100, 100, 90 };
+
+		for (int i = 0; i < score.length; i++) {
 			sum += score[i];
 		}
-		average = sum/(float)score.length;
-		
-		System.out.println("총합 : "+ sum);
-		System.out.println("평균 : "+ average);
+		average = sum / (float) score.length;
+
+		System.out.println("총합 : " + sum);
+		System.out.println("평균 : " + average);
 	}
 
 }
---
+++ src/Chapter05/Ex05_06.java
@@ -0,0 +1,19 @@
+package Chapter05;
+
+public class Ex05_06 {
+
+	public static void main(String[] args) {
+
+		String[] names = { "Kim", "Park", "Yi" };
+
+		for (int i = 0; i < names.length; i++)
+			System.out.println("names[" + i + "] : " + names[i]);
+
+		String tmp = names[2];
+		System.out.println("tmp : " + tmp);
+		names[0] = "Yu";
+
+		for (int i = 0; i < names.length; i++)
+			System.out.println(names[i]);
+	}
+}
