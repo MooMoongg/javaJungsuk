eb5b3b0a40f61a9b9c83b94b45621fa586192132
MooMoongg <alchemist72727@gmail.com>
2025-03-07 21:54:56 +0900

    feat: 오버로딩, 생성자 완료

---
+++ src/Chapter06/Ex06_11.java
@@ -0,0 +1,23 @@
+package Chapter06;
+
+class Data_1{
+	int value;
+}
+
+class Data_2{
+	int value;
+	
+	Data_2(int x){
+		value = x;
+	}
+}
+
+public class Ex06_11 {
+	public static void main(String[] args) {
+		Data_1 d1 = new Data_1();
+		Data_2 d2 = new Data_2(5);
+		
+		System.out.println(d1.value);
+		System.out.println(d2.value);
+	}
+}
---
+++ src/Chapter06/Ex06_12.java
@@ -0,0 +1,30 @@
+package Chapter06;
+
+class Car{
+	String color;
+	String gearType;
+	int door;
+	
+	public Car() {
+	}
+	
+	public Car(String c, String g, int d) {
+		color = c;
+		gearType = g;
+		door = d;
+	}
+}
+
+public class Ex06_12 {
+	public static void main(String[] args) {
+		Car c1 = new Car();
+		c1.color = "white";
+		c1.gearType = "auto";
+		c1.door = 4;
+		
+		Car c2 = new Car("blue", "auto", 2);
+		
+		System.out.println("c1의 color = "+c1.color+", c1의 gearType = "+c1.gearType+", c1의 door = "+c1.door);
+		System.out.println("c2의 color = "+c2.color+", c2의 gearType = "+c2.gearType+", c2의 door = "+c2.door);
+	}
+}
